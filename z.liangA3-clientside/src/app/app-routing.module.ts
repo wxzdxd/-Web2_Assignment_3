868621a1c902a3b951d4ee17c2078f6aab2c363d
wxzdxd <145346264+wxzdxd@users.noreply.github.com>
2024-10-13 17:23:58 +0800

    modify file name

--- z.liangA3-clientside/src/app/app-routing.module.ts
+++ z.liangA3-clientside/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -13,6 +14,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // Use HashLocationStrategy
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
